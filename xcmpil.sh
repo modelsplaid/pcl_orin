8fc5db3b777f67460e45130174b5c596ff57daf5
tzq <tangzhiqiang@yihang.ai>
2022-06-27 10:17:10 +0800

    cuda code can compile, but gpu code cannot compile, have linking error

--- xcmpil.sh
+++ xcmpil.sh
@@ -1,4 +1,5 @@
-export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/home/tzq/disk2/orintestcode/cmaketest/testpkgconfigpc
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/home/tzq/disk2/pc_share/yh-orin/lib/3rdparty/flann1.9.1/lib/pkgconfig
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/home/tzq/disk2/pc_share/yh-orin/lib/3rdparty/eigen3.3.4/share/pkgconfig
 export PATH=${PATH}:"/usr/local/cuda-11.4/bin"
 export ORIN_CFG_PATH=/home/tzq/disk2/pc_share/yh-orin
 export ORIN_THIRD_PARTY=${ORIN_CFG_PATH}/lib/3rdparty
@@ -7,5 +8,5 @@ export flann_DIR=${ORIN_THIRD_PARTY}/flann1.9.1
 mkdir -p build
 cd build
 rm -rf *
-cmake -DWITH_OPENMP=OFF -DWITH_OPENGL=OFF -DWITH_VTK=OFF -DWITH_QT=OFF -DWITH_PCAP=OFF -DWITH_LIBUSB=OFF -DWITH_PNG=OFF -DWITH_QHULL=OFF -DWITH_CUDA=OFF -DWITH_OPENNI=OFF -DWITH_OPENNI2=OFF -Dflann_DIR=/home/tzq/disk2/pc_share/yh-orin/lib/3rdparty/flann1.9.1 .. 
-make -j8 
+cmake -DWITH_OPENMP=OFF -DWITH_OPENGL=OFF -DWITH_VTK=OFF -DWITH_QT=OFF -DWITH_PCAP=OFF -DWITH_LIBUSB=OFF -DWITH_PNG=OFF -DWITH_QHULL=OFF -DWITH_CUDA=ON -DWITH_OPENNI=OFF -DWITH_OPENNI2=OFF .. 
+make  
